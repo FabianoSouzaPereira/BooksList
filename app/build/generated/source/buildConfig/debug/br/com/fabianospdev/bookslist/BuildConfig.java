a204a522f1da7ce13d5573d66d6eaca0d79b2681
Fabiano <fabiano.pereira@seventh.com.br>
2022-10-01 22:01:39 -0300

    Change namespace

--- app/build/generated/source/buildConfig/debug/br/com/fabianospdev/bookslist/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/br/com/fabianospdev/bookslist/BuildConfig.java
@@ -5,7 +5,7 @@ package br.com.fabianospdev.bookslist;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "br.com.seventh.bookslist";
+  public static final String APPLICATION_ID = "br.com.fabianospdev.bookslist";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
